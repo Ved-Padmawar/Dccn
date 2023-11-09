c3658e17a3664f5375641f01f3a68905eeb9c308
thevedp <thevedp@gmail.com>
2023-11-09 12:41:21 +0530

    Scanner to sc

--- SlidingWindow.java
+++ SlidingWindow.java
@@ -4,14 +4,15 @@ public class SlidingWindow
 {
     public static void main(String[] args) 
     {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int window = 0;
 
         System.out.print("Enter Window size: ");
-        window = scanner.nextInt();
+        window = sc.nextInt();
         int sent = 0, ack;
 
-        while (true) {
+        while (true) 
+        {
             for (int i = 0; i < window; i++) 
             {
                 System.out.println("Frame Transmitted " + sent);
@@ -23,7 +24,7 @@ public class SlidingWindow
             }
 
             System.out.print("Enter last received acknowledgment: ");
-            ack = scanner.nextInt();
+            ack = sc.nextInt();
 
             if (ack == window) 
             {
@@ -34,6 +35,6 @@ public class SlidingWindow
                 sent = ack;
             }
         }
-        scanner.close();
+        sc.close();
     }
 }
