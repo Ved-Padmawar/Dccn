67293cecdcb99e1aae74c440340b592aadc6ecbc
thevedp <thevedp@gmail.com>
2023-11-09 12:42:46 +0530

    indented

--- DataLink.java
+++ DataLink.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class DataLink 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter window size: ");
@@ -14,23 +15,29 @@ public class DataLink
         int[] frames = new int[30];
 
         System.out.println("Enter " + f + " frames:");
-        for (int i = 1; i <= f; i++) {
+        for (int i = 1; i <= f; i++) 
+        {
             frames[i] = scanner.nextInt();
         }
 
         System.out.println("\nWith sliding window protocol, the frames will be sent in the following manner (assuming no corruption of frames):\n");
         System.out.println("After sending " + n + " frames at each stage, the sender waits for an acknowledgment sent by the receiver.\n");
 
-        for (int i = 1; i <= f; i++) {
-            if (i % n == 0) {
+        for (int i = 1; i <= f; i++) 
+        {
+            if (i % n == 0) 
+            {
                 System.out.print(frames[i] + " ");
                 System.out.println("\nAcknowledgment of the above frames sent is received by the sender.\n");
-            } else {
+            } 
+            else 
+            {
                 System.out.print(frames[i] + " ");
             }
         }
 
-        if (f % n != 0) {
+        if (f % n != 0) 
+        {
             System.out.println("Acknowledgment of the above frames sent is received by the sender.");
         }
         scanner.close();
